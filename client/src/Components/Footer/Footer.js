7874edb7c52a0e9c5d874d6354b9442487820e84
mcapurri <m.capurri@gmail.com>
2021-05-03 16:30:14 +0200

    re-styling.

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,57 +4,53 @@ import { Link } from 'react-router-dom';
 
 const Footer = (props) => {
     console.log('props', props);
-    return (
+    return !props.isEmployee ? (
         <footer className={style.Footer}>
-            {!props.isEmployee && (
-                <>
-                    <div className={style.Socials}>
-                        <ul>
-                            <li>
-                                <Link to="#">
-                                    <img
-                                        style={{ width: '2rem' }}
-                                        src="/images/twitter-logo.png"
-                                        alt="twitter-logo"
-                                    />{' '}
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="#">
-                                    {' '}
-                                    <img
-                                        style={{ width: '2rem' }}
-                                        src="/images/facebook-logo.png"
-                                        alt="facebook-logo"
-                                    />{' '}
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="#">
-                                    {' '}
-                                    <img
-                                        style={{ width: '2rem' }}
-                                        src="/images/instagram-logo.png"
-                                        alt="instagram-logo"
-                                    />{' '}
-                                </Link>
-                            </li>
-                        </ul>
-                        <h5> Follow Us </h5>
-                    </div>
-                    {props.width < '860' && (
-                        <div className={style.Infos}>
-                            <address className="address">
-                                Animal Clinic <br /> Buschkrug Allee 206, <br />{' '}
-                                12359 Berlin <br /> Germany <br />{' '}
-                                <small>+49 157 00 00 00</small>
-                            </address>
-                        </div>
-                    )}
-                </>
+            <div className={style.Socials}>
+                <ul>
+                    <li>
+                        <Link to="#">
+                            <img
+                                style={{ width: '2rem' }}
+                                src="/images/twitter-logo.png"
+                                alt="twitter-logo"
+                            />{' '}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                            {' '}
+                            <img
+                                style={{ width: '2rem' }}
+                                src="/images/facebook-logo.png"
+                                alt="facebook-logo"
+                            />{' '}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                            {' '}
+                            <img
+                                style={{ width: '2rem' }}
+                                src="/images/instagram-logo.png"
+                                alt="instagram-logo"
+                            />{' '}
+                        </Link>
+                    </li>
+                </ul>
+                <h5> Follow Us </h5>
+            </div>
+            {props.width < '860' && (
+                <div className={style.Infos}>
+                    <address className="address">
+                        Animal Clinic <br /> Buschkrug Allee 206, <br /> 12359
+                        Berlin <br /> Germany <br />{' '}
+                        <small>+49 157 00 00 00</small>
+                    </address>
+                </div>
             )}
         </footer>
-    );
+    ) : null;
 };
 
 export default Footer;
