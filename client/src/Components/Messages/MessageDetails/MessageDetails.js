5012f0e5af78455bb3e4e77d506d9afbf894dc3a
mcapurri <m.capurri@gmail.com>
2021-05-18 07:50:05 +0200

    styling

--- client/src/Components/Messages/MessageDetails/MessageDetails.js
+++ client/src/Components/Messages/MessageDetails/MessageDetails.js
@@ -93,14 +93,6 @@ const MessageDetails = (props) => {
                             <b style={{ color: 'rgb(92, 41, 168)' }}>
                                 Received on:{' '}
                             </b>
-                            {/* {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: '2-digit',
-                            day: '2-digit',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            second: '2-digit',
-                        }).format(createdAt)} */}
                             {createdAt.substring(0, 10)}
                         </p>
                         {appointment && (
--- client/src/Components/Users/EditUser/EditUser.js
+++ client/src/Components/Users/EditUser/EditUser.js
@@ -4,93 +4,93 @@ import { Form } from 'react-bootstrap';
 
 const EditUser = (props) => {
     return (
-        <div className={style.Container}>
-            <Form className={style.Form} onSubmit={props.handleSubmit}>
-                <Form.Group className={style.Group}>
-                    <Form.Label className={style.Label} htmlFor="firstName">
-                        Name: &nbsp;
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="firstName"
-                        name="firstName"
-                        value={props.firstName}
-                        onChange={(e) => props.handleChange(e)}
-                    />
-                </Form.Group>
+        // <div className={style.Container}>
+        <Form className={style.Form} onSubmit={props.handleSubmit}>
+            <Form.Group className={style.Group}>
+                <Form.Label className={style.Label} htmlFor="firstName">
+                    Name: &nbsp;
+                </Form.Label>
+                <Form.Control
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    value={props.firstName}
+                    onChange={(e) => props.handleChange(e)}
+                />
+            </Form.Group>
 
-                <Form.Group className={style.Group}>
-                    <Form.Label className={style.Label} htmlFor="lastName">
-                        Last name: &nbsp;
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="lastName"
-                        name="lastName"
-                        value={props.lastName}
-                        onChange={(e) => props.handleChange(e)}
-                    />
-                </Form.Group>
-                <Form.Group className={style.Group}>
-                    <Form.Label className={style.Label} htmlFor="street">
-                        Street: &nbsp;
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="street"
-                        name="street"
-                        value={props.street}
-                        onChange={(e) => props.handleChange(e)}
-                    />
-                </Form.Group>
+            <Form.Group className={style.Group}>
+                <Form.Label className={style.Label} htmlFor="lastName">
+                    Last name: &nbsp;
+                </Form.Label>
+                <Form.Control
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    value={props.lastName}
+                    onChange={(e) => props.handleChange(e)}
+                />
+            </Form.Group>
+            <Form.Group className={style.Group}>
+                <Form.Label className={style.Label} htmlFor="street">
+                    Street: &nbsp;
+                </Form.Label>
+                <Form.Control
+                    type="text"
+                    id="street"
+                    name="street"
+                    value={props.street}
+                    onChange={(e) => props.handleChange(e)}
+                />
+            </Form.Group>
 
-                <Form.Group className={style.Group}>
-                    <Form.Label className={style.Label} htmlFor="zipCode">
-                        ZIP Code: &nbsp;
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="zipCode"
-                        name="zipCode"
-                        value={props.zipCode}
-                        onChange={(e) => props.handleChange(e)}
-                    />
-                </Form.Group>
+            <Form.Group className={style.Group}>
+                <Form.Label className={style.Label} htmlFor="zipCode">
+                    ZIP Code: &nbsp;
+                </Form.Label>
+                <Form.Control
+                    type="text"
+                    id="zipCode"
+                    name="zipCode"
+                    value={props.zipCode}
+                    onChange={(e) => props.handleChange(e)}
+                />
+            </Form.Group>
 
-                <Form.Group className={style.Group}>
-                    <Form.Label className={style.Label} htmlFor="city">
-                        City: &nbsp;
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="city"
-                        name="city"
-                        value={props.city}
-                        onChange={(e) => props.handleChange(e)}
-                    />
-                </Form.Group>
+            <Form.Group className={style.Group}>
+                <Form.Label className={style.Label} htmlFor="city">
+                    City: &nbsp;
+                </Form.Label>
+                <Form.Control
+                    type="text"
+                    id="city"
+                    name="city"
+                    value={props.city}
+                    onChange={(e) => props.handleChange(e)}
+                />
+            </Form.Group>
 
-                <Form.Group className={style.Group}>
-                    <Form.Label className={style.Label} htmlFor="phoneNum">
-                        Phone Num: &nbsp;
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="phoneNumber"
-                        name="phoneNumber"
-                        value={props.phoneNumber}
-                        onChange={(e) => props.handleChange(e)}
-                    />
-                </Form.Group>
+            <Form.Group className={style.Group}>
+                <Form.Label className={style.Label} htmlFor="phoneNum">
+                    Phone Num: &nbsp;
+                </Form.Label>
+                <Form.Control
+                    type="text"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    value={props.phoneNumber}
+                    onChange={(e) => props.handleChange(e)}
+                />
+            </Form.Group>
 
-                <div className={style.buttons}>
-                    <button onClick={props.toggleEditForm}>Back</button>
-                    <button style={{ marginRight: '10%' }} type="submit">
-                        Update
-                    </button>
-                </div>
-            </Form>
-        </div>
+            <div className={style.buttons}>
+                <button onClick={props.toggleEditForm}>Back</button>
+                <button style={{ marginRight: '10%' }} type="submit">
+                    Update
+                </button>
+            </div>
+        </Form>
+        // </div>
     );
 };
 
